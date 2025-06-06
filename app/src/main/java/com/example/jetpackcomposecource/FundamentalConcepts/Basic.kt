23f03d294a451c95f7f2d4c45f6affebf82014ba
hardikdigital111 <hardik.digitalskylink@gmail.com>
2025-06-06 10:55:27 +0530

    first commit

---
+++ app/src/main/java/com/example/jetpackcomposecource/FundamentalConcepts/Basic.kt
@@ -0,0 +1,24 @@
+package com.example.jetpackcomposecource.FundamentalConcepts
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcomposecource.ui.theme.Purple40
+
+
+@Composable
+fun Greeting(){
+    Column {
+
+        Text(text = "Hello Jetpack Compose", color = Purple40)
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun GreetingPreview(){
+    Greeting()
+}
+
+
---
+++ app/src/main/java/com/example/jetpackcomposecource/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.jetpackcomposecource
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.jetpackcomposecource.FundamentalConcepts.Greeting
+import com.example.jetpackcomposecource.ui.theme.JetpackComposeCourceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeCourceTheme { Greeting()   }
+        }
+    }
+}
